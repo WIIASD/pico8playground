0a688c8c1846e18b878405d3c64c3b7f464bcc89
WIIASD <liziyang486@gmail.com>
2022-11-30 04:58:35 -0500

    clean up, followed tutorial until clipping

--- 3d/main.lua
+++ 3d/main.lua
@@ -1,25 +1,3 @@
-function create_triangle(vertices)
-    local r = {}
-    r.vertices = vertices
-    r.draw = function(self)
-        for i=1, 3, 1 do
-            local np = i + 1
-            if i + 1 > 3 then
-                np = 1
-            end
-            line(self.vertices[i].x, self.vertices[i].y, self.vertices[np].x, self.vertices[np].y)
-        end
-    end
-    r.copy = function(self)
-        return create_triangle({
-            self.vertices[1]:copy(), 
-            self.vertices[2]:copy(), 
-            self.vertices[3]:copy()
-        })
-    end
-    return r
-end
-
 function create_mesh(tris)
     local r = {}
     r.tris = tris
@@ -30,40 +8,36 @@ function tan(a)
     return sin(a)/cos(a)
 end
 
-mesh = {}
-
-screen_w=128
-screen_h=128
-a = screen_w/screen_h
-fov = 0.25
-znear = 0.1
-zfar = 1000
-angle = 0
-vCamera = create_vec3d(0,0,0)
-
-rotate_x = {}
-rotate_y = {}
-rotate_z = {}
-
---perspective projection matrix
---4x4 because we need to store the z value in the w place of the vector, and divide all other entries of the resulted vector by z
-project_mat = {
-    {a/tan(fov/2), 0, 0, 0},
-    {0, 1/tan(fov/2), 0, 0},
-    {0, 0, zfar/(zfar - znear), 1},
-    {0, 0, (-zfar * znear)/(zfar - znear), 0}
-}
-
 function project(v3d, project_mat)
-    local p_mat = v3d:tomat()
-    add(p_mat[1], 1)
-    local r = matmul(p_mat, project_mat)
-    if r[1][4]~=0 then
-        r[1][1] /= r[1][4]
-        r[1][2] /= r[1][4]
-        r[1][3] /= r[1][4]
+    local r = v3d:matmul(project_mat)
+    if r.w~=0 then
+        r = r:scale(1/r.w)
     end
-    return create_vec3d(r[1][1], r[1][2], r[1][3])
+    return r
+end
+
+function pointat_mat(pos, v_lookdir, v_up, v_right)
+    return {
+        {v_right.x, v_right.y, v_right.z, 0},
+        {v_up.x, v_up.y, v_up.z, 0},
+        {v_lookdir.x, v_lookdir.y, v_lookdir.z, 0},
+        {pos.x, pos.y, pos.z, 1}
+    }
+end
+
+--inverse (only) the point at matrix
+function pointat_inv(pointat_mat)
+    local m = pointat_mat
+    local a = create_vec3d(m[1][1],m[1][2],m[1][3])
+    local b = create_vec3d(m[2][1],m[2][2],m[2][3])
+    local c = create_vec3d(m[3][1],m[3][2],m[3][3])
+    local t = create_vec3d(m[4][1],m[4][2],m[4][3])
+    return{
+        {a.x,b.x,c.x,0},
+        {a.y,b.y,c.y,0},
+        {a.z,b.z,c.z,0},
+        {-t:dot(a),-t:dot(b),-t:dot(c),1}
+    }
 end
 
 function _init()
@@ -94,72 +68,119 @@ function _init()
     })
 end
 
-function _update60()
+mesh = {}
 
-    rotate_x = {
-        {1,0,0},
-        {0, cos(angle), -sin(angle)},
-        {0, sin(angle), cos(angle)},
-    }     
-    rotate_y = {
-        {cos(angle), 0, sin(angle)},
-        {0, 1, 0},
-        {-sin(angle), 0, cos(angle)}
-    }  
-    rotate_z = {
-        {cos(angle), -sin(angle),0},
-        {sin(angle), cos(angle),0},
-        {0,0,1}
-    }
-    
-    angle+=0.002
+screen_w=128
+screen_h=128
+a = screen_w/screen_h
+fov = 0.25
+znear = 0.1
+zfar = 1000
+angle = 0
+yaw = 0
+xaw = 0
+v_camera = create_vec3d(0,0,0)
+v_lookdir = create_vec3d(0,0,1)
+v_up = create_vec3d(0,1,0)
+v_right = v_up:cross(v_lookdir)
+
+--perspective projection matrix
+--4x4 because we need to store the z value in the w place of the vector, and divide all other entries of the resulted vector by z
+project_mat = {
+    {a/tan(fov/2), 0, 0, 0},
+    {0, 1/tan(fov/2), 0, 0},
+    {0, 0, zfar/(zfar - znear), 1},
+    {0, 0, (-zfar * znear)/(zfar - znear), 0}
+}
+
+function _update60()
+    v_forward = create_vec3d(1,0,0):scale(v_lookdir.x)
+                                   :add(create_vec3d(0,0,1):scale(v_lookdir.z))
+                                   :normalized()
+                                   :scale(0.1) 
+
+    if btn(0) then
+        v_camera = v_camera:add(v_right:scale(0.1))
+    elseif btn(1) then
+        v_camera = v_camera:sub(v_right:scale(0.1))
+    end
+
+    if btn(2) then
+        v_camera = v_camera:add(v_forward)
+    elseif btn(3) then
+        v_camera = v_camera:sub(v_forward)
+    end
+
+    if btn(0,1) then
+        yaw=0.005
+    elseif btn(1,1) then
+        yaw=-0.005
+    else
+        yaw=0
+    end
+    if btn(2,1) then
+        xaw=-0.005
+    elseif btn(3,1) then
+        xaw=0.005
+    else
+        xaw=0
+    end
+
 end
 
 function _draw()
     cls(1)
+    --rotate look direction
+    v_lookdir = v_lookdir:rotate_around(yaw, v_up):normalized()
+    v_lookdir = v_lookdir:rotate_around(xaw, v_right):normalized()
+    
+    --update new up and right vector based on rotated look direction
+    local a = v_lookdir:scale(v_up:dot(v_lookdir))
+    v_up = v_up:sub(a):normalized()
+    v_right = v_up:cross(v_lookdir) 
+
+    local mat_camera = pointat_mat(v_camera, v_lookdir, v_up, v_right)
+
+    local mat_view = pointat_inv(mat_camera)
+
     for tri in all(mesh.tris) do
 
         local tri_tmp = tri:copy()
 
         --rotate
-        for i=1, 3, 1 do
-            tri_tmp.vertices[i] = tri_tmp.vertices[i]:rotate(rotate_x)
-            tri_tmp.vertices[i] = tri_tmp.vertices[i]:rotate(rotate_y)
-            tri_tmp.vertices[i] = tri_tmp.vertices[i]:rotate(rotate_z)
-        end
+        tri_tmp = tri_tmp:rotate(angle, angle, angle)
 
-        --translate
-        for i=1, 3, 1 do
-            tri_tmp.vertices[i].z += 2
-        end
-        
-        --normal
-        local line1 = tri_tmp.vertices[2]:sub(tri_tmp.vertices[1])
-        local line2 = tri_tmp.vertices[3]:sub(tri_tmp.vertices[1])
+        --z offset into the screen
+        tri_tmp = tri_tmp:add(create_vec3d(0,0,2))
 
-        local normal = line1:cross(line2):normalized()
-
+        --front face culling
         --note: dot product (between camera and triangle normal) < 0 means the similarity between their directions is low, since the light cannot be reflected on the surface if the surface is facing at the same direction as the light source
-        local camera2v = tri_tmp.vertices[1]:sub(vCamera)
-        if normal:dot(camera2v) < 0 then
-            --project
-            local projected_vertices = {
-                project(tri_tmp.vertices[1], project_mat),
-                project(tri_tmp.vertices[2], project_mat),
-                project(tri_tmp.vertices[3], project_mat)
-            }
-            tri_tmp = create_triangle(projected_vertices)
-    
-            --offset and scale
-            for i=1, 3, 1 do
-                tri_tmp.vertices[i].x+=1
-                tri_tmp.vertices[i].y+=1
-                tri_tmp.vertices[i].x*=0.5 * screen_w
-                tri_tmp.vertices[i].y*=0.5 * screen_h
-            end
-    
-            tri_tmp:draw()
+        local camera2v = tri_tmp.vertices[1]:sub(v_camera)
+        --cannot see triangle, not rendering
+        if tri_tmp.normal:dot(camera2v) >= 0 then
+            goto continue
         end
 
+        ------------------Render-----------------------
+
+        --project to view space
+        tri_tmp = tri_tmp:matmul(mat_view)
+
+        --project to screen space
+        local projected_vertices = {
+            project(tri_tmp.vertices[1], project_mat),
+            project(tri_tmp.vertices[2], project_mat),
+            project(tri_tmp.vertices[3], project_mat)
+        }
+        tri_tmp = create_triangle(projected_vertices)
+
+        --offset and scale
+        tri_tmp = tri_tmp:add(create_vec3d(1,1,0))
+                         :mul(create_vec3d(0.5 * screen_w, 0.5 * screen_h, 1))
+
+        tri_tmp:draw()
+
+        ::continue::
     end
+    --angle+=0.004
 end
---
+++ 3d/triangle.lua
@@ -0,0 +1,65 @@
+function create_triangle(vertices)
+    --private
+    local calculate_normal = function(vertices)
+        local line1 = vertices[2]:sub(vertices[1])
+        local line2 = vertices[3]:sub(vertices[1])
+
+        return line1:cross(line2):normalized()
+    end
+
+    local r = {}
+    r.vertices = vertices
+    r.normal = calculate_normal(r.vertices)
+    r.draw = function(self)
+        for i=1, 3, 1 do
+            local np = i + 1
+            if i + 1 > 3 then
+                np = 1
+            end
+            line(self.vertices[i].x, self.vertices[i].y, self.vertices[np].x, self.vertices[np].y)
+        end
+    end
+    r.copy = function(self)
+        return create_triangle({
+            self.vertices[1]:copy(), 
+            self.vertices[2]:copy(), 
+            self.vertices[3]:copy()
+        })
+    end
+    r.rotate = function(self, a, b, c)
+        return create_triangle({
+            self.vertices[1]:rotate(a,b,c),
+            self.vertices[2]:rotate(a,b,c),
+            self.vertices[3]:rotate(a,b,c)
+        })
+    end
+    r.add = function(self, v)
+        return create_triangle({
+            self.vertices[1]:add(v),
+            self.vertices[2]:add(v),
+            self.vertices[3]:add(v)
+        })
+    end
+    r.scale = function(self, n)
+        return create_triangle({
+            self.vertices[1]:scale(n),
+            self.vertices[2]:scale(n),
+            self.vertices[3]:scale(n)
+        })
+    end
+    r.mul = function(self, v)
+        return create_triangle({
+            self.vertices[1]:mul(v),
+            self.vertices[2]:mul(v),
+            self.vertices[3]:mul(v)
+        })
+    end
+    r.matmul = function(self, m)
+        return create_triangle ({
+            self.vertices[1]:matmul(m),
+            self.vertices[2]:matmul(m),
+            self.vertices[3]:matmul(m)
+        })
+    end
+    return r
+end
